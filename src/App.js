80843fb95478e08e5dc4b6f1e4265718e26b6894
DJ Bowers <dj@dj-bowers.com>
2020-02-20 09:24:40 -0800

    prepare app for checkout page
    also add empty message to cart-dropdown

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import { createStructuredSelector } from 'reselect';
 
 import './App.scss';
 
-import Header from './components/header/header.component';
 import HomePage from './pages/home/home.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/signin/sign-in-and-sign-up.component';
+import CheckoutPage from './pages/checkout/checkout.component';
+
+import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
@@ -47,6 +49,7 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
+          <Route exact path="/checkout" component={CheckoutPage} />
           <Route
             exact
             path="/signin"
