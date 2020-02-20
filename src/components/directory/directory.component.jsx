e812492d89b60a83bf9d19e4bf2869b3092acef5
DJ Bowers <dj@dj-bowers.com>
2020-02-20 14:53:00 -0800

    add directory data to redux

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import { selectDirectorySections } from './../../redux/directory/directory.selectors';
+
 import './directory.styles.scss';
+
 import MenuItem from '../menu-item/menu-item.component';
-import DIRECTORY_DATA from './directory.data';
 
-export default class Directory extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      sections: DIRECTORY_DATA
-    };
-  }
+const Directory = ({ sections }) => {
+  return (
+    <div className="directory-menu">
+      {sections.map(({ id, ...otherSectionProps }) => (
+        <MenuItem key={id} {...otherSectionProps} />
+      ))}
+    </div>
+  );
+};
 
-  render() {
-    return (
-      <div className="directory-menu">
-        {this.state.sections.map(({ id, ...otherSectionProps }) => (
-          <MenuItem key={id} {...otherSectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections
+});
+
+export default connect(mapStateToProps)(Directory);
